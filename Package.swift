632f4190669b83f0f9511f6bc57ac0f0464dfbe7
Pedro Cavaleiro <pm.cavaleiro@hotmail.com>
2021-05-30 02:53:01 +0100

    Fixed relative path

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "APIAppAuthentication",
     dependencies: [
         // This just points to the SwiftPM at the root of this repository.
-        .package(path: "/src/Swift/APIAppAuthentication"),
+        .package(path: "src/Swift/APIAppAuthentication"),
         // You will want to depend on a stable semantic version instead:
         // .package(url: "https://github.com/apple/swift-package-manager", .exact("0.4.0"))
     ],
