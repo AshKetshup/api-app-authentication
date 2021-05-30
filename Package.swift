b837a13408d07365504d9b97e28fdcc2594b2ac2
Pedro Cavaleiro <pm.cavaleiro@hotmail.com>
2021-05-30 02:48:25 +0100

    Added root package

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "package-info",
+    dependencies: [
+        // This just points to the SwiftPM at the root of this repository.
+        .package(path: "../../"),
+        // You will want to depend on a stable semantic version instead:
+        // .package(url: "https://github.com/apple/swift-package-manager", .exact("0.4.0"))
+    ],
+    targets: [
+        .target(
+            name: "package-info",
+            dependencies: ["SwiftPM"]),
+    ]
+)
