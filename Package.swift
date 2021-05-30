1cafb18200bb9a166bc369eccf5d1c31020ddf3b
Pedro Cavaleiro <pm.cavaleiro@hotmail.com>
2021-05-30 02:49:25 +0100

    Added local resolution

--- Package.swift
+++ Package.swift
@@ -1,16 +1,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "package-info",
+    name: "APIAppAuthentication",
     dependencies: [
         // This just points to the SwiftPM at the root of this repository.
-        .package(path: "../../"),
+        .package(path: "/src/Swift/APIAppAuthentication"),
         // You will want to depend on a stable semantic version instead:
         // .package(url: "https://github.com/apple/swift-package-manager", .exact("0.4.0"))
     ],
     targets: [
         .target(
-            name: "package-info",
-            dependencies: ["SwiftPM"]),
+            name: "APIAppAuthentication"
     ]
 )
